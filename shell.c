8dd83f0b504f81eb72d6af03562f4a3fc11675a6
Vishal Kushwaha <vishsinkush@gmail.com>
2020-10-02 19:24:51 +0000

    working multipipe and debugging statements

--- shell.c
+++ shell.c
@@ -2,16 +2,20 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<string.h>
-#include <sys/wait.h>
+#include<ctype.h>
+#include<sys/wait.h>
 
 #define STDIN 0
 #define STDOUT 1
+
+#define DEBUG 3
 
 char* get_input(){
     char *buf;
     size_t size = 1024;
     buf = (char*) malloc(size);
     printf("$ ");
+    fflush(stdout);
     if (getline(&buf, &size, stdin) == -1){
             exit(EXIT_FAILURE);
     }
@@ -95,77 +99,145 @@ int count_pipe(char *input){
     return count;
 }
 
+void close_safe(int fd){
+    if(close(fd) == -1){
+        perror("close");
+    } else {
+        #if DEBUG > 2
+        fprintf(stderr, "( -x- ) %d closed fd (%d)\n", getpid(), fd);
+        #endif
+    }
+}
 
+void dup2_safe(int fd1, int fd2){
+    if(dup2(fd1, fd2) == -1){
+        perror("dup2");
+    } else {
+        #if DEBUG > 2
+        fprintf(stderr, "( dup ) %d dup2ed fd (%d, %d)\n", getpid(), fd1, fd2);
+        #endif
+    }
+}
 
-int execute(char **args, int *inputPipe, int *outputPipe){
+void pipe_safe(int *fd){
+    if (pipe(fd) == -1){
+        perror("pipe");
+    } else {
+        #if DEBUG > 1
+        printf("( ==> ) %d pipped fd (%d, %d)\n", getpid(), fd[0], fd[1]);
+        #endif
+    }
+}
+
+int **init_pipes(int numPipes){
+    // printf("=====> Initializing %d pipe(s) for pid %d\n", numPipes, getpid());
+    int **pipes = malloc(numPipes*sizeof(int*));
+    for(int i = 0; i<numPipes; ++i){
+        pipes[i] = malloc(2*sizeof(int));
+        pipe_safe(pipes[i]);
+    } 
+    // printf("=====> Done\n");
+    return pipes;
+}
+
+int execute_pipe(char **args, int *inputPipe, int *outputPipe){
     /*
-        Executes the simple command without any pipes 
-    */
-    pid_t pid, wpid;
-    int status;
-        
-    pid = fork();
-    if(pid == 0){
-        
-        if (inputPipe != NULL){
-            close(inputPipe[1]);
-            if (dup2(inputPipe[0], STDIN) == -1){
-                perror("dup2");
-            } close(inputPipe[0]);
+        Generic execution command with/without pipes
+        in case pipes are used:
+            0. Closes unneeded pipe ends.
+            1. it duplicates the pipefd to STDIN/OUT
+
+    */    
+    int childPid = fork();
+    if (childPid == 0){
+        #if DEBUG > 1
+        printf("( --< ) %d forked child (%d, \"%s\")\n", getppid(), getpid(), args[0]);
+        #endif
+        if (inputPipe){
+            close_safe(inputPipe[1]);
+            dup2_safe(inputPipe[0], STDIN);
         }
-        if (outputPipe != NULL){
-            close(outputPipe[0]);
-            if (dup2(outputPipe[1], STDOUT) == -1){
-                perror("dup2");
-            } close(outputPipe[1]);
+        if (outputPipe){
+            close_safe(outputPipe[0]);
+            dup2_safe(outputPipe[1], STDOUT);
         }
-        printf("executing %s\n", args[0]);
         execvp(args[0], args);
         perror("exec_error");
         exit(EXIT_FAILURE);
-    } else if(pid < 0){
+    } 
+    else if (childPid < 0){
         perror("fork");
-    }else{
-        wpid = waitpid(pid, &status, WUNTRACED);
     }
-    return 1;
+    return childPid;
 }
 
-
-int** init_pipes(int pipeCount){
-    int ** pfds = malloc(pipeCount * sizeof(int *));
-    for(int i = 0 ; i < pipeCount; ++i){
-        pfds[i] = malloc(2 * sizeof(int));
-        if(pipe(pfds[i]) == -1){
-            perror("pipe");
-        }
-    }
-    return pfds;
+int execute(char **args){
+    #if DEBUG
+    printf("[Executing \"%s\"]\n",args[0]);
+    #endif
+    int status = execute_pipe(args, NULL, NULL); 
+    #if DEBUG
+    printf("[Done]\n",args[0]);
+    #endif
+    return status;
 }
 
-int execute_pipe(char **commands, int **fds, int pipeCount){
+char *remove_space(char *str){
     /*
-        inprogress...
+        Removes whitespace from both ends of a string, 
+        this function modifies the existing string.
     */
-    printf("in execute_pipe, pipe(s) = %d\n", pipeCount);
-    // need to pass '-' as input for stdinputw
-    for(int i = 0; i <= pipeCount; ++i){
-        char** args = tokenize(commands[i]);
-        int *inputPipe = NULL, *outputPipe = NULL;
-        
-        if(i == 0){
-            outputPipe = fds[0];
-        } else if(i == pipeCount){
-            inputPipe = fds[i-1];
-        } else {
-            inputPipe = fds[i-1];
-            outputPipe = fds[i];
-        }
+    int start = 0, i;
+    int end = strlen(str) - 1;
+    while (isspace((unsigned char) str[start])){
+        start++;
+    }
+    while ((end >= start) && isspace((unsigned char) str[end])){
+        end--;
+    }
+    for (i = start; i <= end; i++){
+        str[i - start] = str[i];
+    }
+    str[i - start] = '\0';
+    return str;
+}
 
-        execute(args, inputPipe, outputPipe);
-        
-        // free(args);
-   }
+int execute_multipipe(char **commands, int pipeCount){
+    /*
+        Executes each command using pipes as follows:
+        0. Initializes all pipes.
+        1. Waits for previous process (if any) to complete.
+        2. Strips whitespace and tokenizes command to be executed.
+        3. Executes the command by passing either a pipe or NULL to execute_pipe.
+        4. closes last pipe fds.
+    */
+    #if DEBUG
+    printf("[Executing multipipe, pipe(s):%d]\n", pipeCount);
+    #endif
+    int  ** pipes = init_pipes(pipeCount);
+    char ** args  = tokenize(remove_space(commands[0]));
+    int     pid   = execute_pipe(args, NULL, pipes[0]);
+    free(args);
+    for(int i = 1; i < pipeCount; ++i){
+        waitpid(pid, NULL, 0);
+        args = tokenize(remove_space(commands[i]));
+        pid  = execute_pipe(args, pipes[i-1], pipes[i]);
+        close_safe(pipes[i-1][0]);
+        close_safe(pipes[i-1][1]);
+        // free(pipes[i-1]);
+    }
+    waitpid(pid, NULL,0);
+    args = tokenize(remove_space(commands[pipeCount]));
+    pid  = execute_pipe(args, pipes[pipeCount - 1], NULL);
+    close_safe(pipes[pipeCount - 1][0]);
+    close_safe(pipes[pipeCount - 1][1]);
+    //free(args);
+    // free(pipes[pipeCount-1]);
+    // free(pipes);
+    waitpid(pid, NULL, 0);
+    #if DEBUG
+    printf("[Done]\n");
+    #endif
     return 1;
 }
 
@@ -180,7 +252,7 @@ void cmd_loop(){
     char *input;
     char **args;
     char **commands;
-    int status, pipeCount, **pipefds;
+    int status, pipeCount;
 
     do{
         input = get_input();
@@ -188,18 +260,14 @@ void cmd_loop(){
 
         if(pipeCount == 0){
             args = tokenize(input); 
-            status = execute(args, NULL, NULL);
+            status = execute(args);
+            free(args);
         }else{
-            printf("pipe(s) : %d\n", pipeCount);
             commands = parse_input(input);
-            pipefds = init_pipes(pipeCount);
-            status = execute_pipe(commands, pipefds, pipeCount);
+            status = execute_multipipe(commands, pipeCount);
             free(commands);
-            free(pipefds);
         }
-
         free(input);
-        free(args);
     }while(1);
 }
 
